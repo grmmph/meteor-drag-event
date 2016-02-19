032b52abbe26fa17d58b053befafe50a46cea603
Yonatan Wolowelsky <grmmph@gmail.com>
2016-02-19 03:04:17 +0100

    add to meteor package

---
+++ meteor-drag-event/README.md
@@ -0,0 +1,36 @@
+# Meteor Drag Event
+
+## What's that?
+
+This package extend Meteor Blaze Events to support drag event.
+
+Now you can use `drag` event just like you would use `click` event on the events template helpers.
+
+## Usage
+
+When using the drag event, the first event argument for the event handler callback will include a `drag` object with
+ - `type <string>`: `dragstart`, `dragend` and `dragging`
+ - `dx<Number>`: Difference between last x and current x
+ - `dy<Number>`: Difference between last y and current y
+
+### Example
+```
+Template.hello.events({
+  'drag .my-draggable-element': function (evt) {
+
+    if (evt.drag.type === 'dragstart') {
+      console.log('You start dragging!')
+    } else if (evt.drag.type === 'dragend') {
+      console.log('You stopped dragging!')
+    } else if (evt.drag.type === 'dragend') {
+      console.log('You are dragging!')
+    }
+
+    // Move elements in space (element must have position: absolute)
+    $(evt.target).animate({
+      left: '+=' + evt.drag.dx,
+      top: '+=' + evt.drag.dy,
+    }, 1);
+  }
+});
+```
---
+++ meteor-drag-event/meteor-drag-event.js
@@ -0,0 +1,105 @@
+(function () {
+  var MeteorDragEvent = class MeteorDragEvent {
+    constructor(selector, oldHandler) {
+      this.oldHandler = oldHandler;
+      this.selector = selector;
+      this.dragEvent = new CustomEvent('drag');
+
+      this.selected;
+      this.posX = 0;
+      this.posY = 0;
+      this.dx = 0;
+      this.dy = 0;
+      this.type;
+      this.elements = $(this.selector);
+      this.element;
+
+      this.isFirstDragging = true;
+      this.setEvents();
+    };
+
+    dragstart(elem, evt) {
+      this.dragType = 'dragstart';
+      this.handler(evt);
+      this.selected = elem;
+    }
+
+    dragging(evt) {
+      if (!this.selected) {
+        return;
+      }
+
+      newXPos = document.all ? window.event.clientX : evt.pageX;
+      newYPos = document.all ? window.event.clientY : evt.pageY;
+
+      if (this.isFirstDragging) {
+        this.dx = 0;
+        this.dy = 0;
+        this.isFirstDragging = false;
+      } else {
+        this.dx = newXPos - this.posX;
+        this.dy = newYPos - this.posY;
+      }
+
+      this.posX = newXPos;
+      this.posY = newYPos;
+
+      this.dragType = 'dragging';
+      this.element.trigger('drag');
+    }
+
+    dragend() {
+      this.dragType = 'dragend';
+      this.selected = null;
+      this.isFirstDragging = true;
+      this.element.trigger('drag');
+    }
+
+    handler() {
+      var evt = arguments[0];
+      evt.drag = {};
+      evt.drag.dx = this.dx;
+      evt.drag.dy = this.dy;
+      evt.drag.type = this.dragType;
+      return this.oldHandler.apply(this, arguments);
+    }
+
+    setEvents() {
+      var self = this;
+      this.elements.on("drag", function (evt) {
+          self.handler(evt);
+      });
+
+      this.elements.on('mousedown', function (evt) {
+        self.element = $(this);
+        self.dragstart(this, evt);
+        return false;
+      });
+
+      document.onmousemove = function (evt) {
+        self.dragging(evt);
+      };
+
+      document.onmouseup = function (evt) {
+        self.dragend(evt);
+      }
+    }
+  };
+
+  try {
+    Blaze._EventSupport.eventsToDelegate.drag = 1;
+    var oldDelegateEvents = Blaze._DOMBackend.Events.delegateEvents;
+    Blaze._DOMBackend.Events.delegateEvents = function (elem, type, selector, handler) {
+      if (type === 'drag') {
+        $(document).ready(function () {
+          new MeteorDragEvent(selector, handler);
+        });
+      } else {
+        oldDelegateEvents.apply(this, arguments);
+      }
+    }
+  }
+  catch (err) {
+    console.log(err);
+  }
+})();
